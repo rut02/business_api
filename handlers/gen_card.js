d9a1ca5bcbef9fe3a2fceab9bb1fa1cc997b2ef8
Your Name <you@example.com>
2024-08-02 15:27:04 +0700

    gencard

--- handlers/gen_card.js
+++ handlers/gen_card.js
@@ -30,8 +30,9 @@ async function drawCard(data, outputPath) {
 
     // วาดข้อมูล
     ctx.fillStyle = '#333';
-    ctx.font = 'bold 20px Arial';
+    ctx.font = 'bold 20px tahoma';
     ctx.fillText(`${data.firstname} ${data.lastname}`, 140, 40);
+    console.log(data.firstname, data.lastname);
 
     ctx.font = '16px Arial';
     ctx.fillText(`Position: ${data.position}`, 140, 70);
